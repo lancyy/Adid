46841b29cba92a4056bfd82f4fe10ea2df0f071a
Kamil <kam.mikulski@gmail.com>
2018-01-12 20:52:01 +0100

    more code

---
+++ src/f/asfasf.java
@@ -0,0 +1,27 @@
+package f;
+
+public class asfasf {
+
+	public static void main(String[] args) {
+		
+	
+		
+		String carBrand = "Audi";
+		String carModel = "A4";
+		short carYear = 2007;
+		float CarPrice = 67000f;
+		
+		System.out.println("Wybrany samochód to");
+		System.out.print(carBrand);
+		System.out.print(" ");
+		System.out.print(carModel);
+		System.out.print(", rocznik ");
+		System.out.print(carYear);
+		System.out.print(", cena ");
+		System.out.print(CarPrice);
+		System.out.println("z³");
+	}
+
+}
+
+}
